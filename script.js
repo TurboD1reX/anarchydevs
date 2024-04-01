2660d823c4a1a5de60ff1d762916470815c6a9d4
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2024-04-01 20:28:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function about(){
+    const about = document.querySelector('.aboutUsContainer');
+    about.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+function contact(){
+    const form = document.querySelector('.form');
+    form.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
